e2abf440d984eec6b0174563b4aee8044a2722ee
BekzhanK1 <bkimadieff@gmail.com>
2025-10-08 19:54:27 +0500

    make healthcheck public

--- common/views.py
+++ common/views.py
@@ -1,11 +1,13 @@
 from django.db import connection
 from django.http import JsonResponse
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 
 @api_view(['GET'])
+@permission_classes([AllowAny])
 def health_check(request):
     """
     Health check endpoint that verifies the application and database are operational.
